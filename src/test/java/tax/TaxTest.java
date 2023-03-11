ebc688e2316a3bf3bcc9e92a004dc56bf92b5652
GagaWag <lll3gmslll@gmail.com>
2023-03-11 19:28:16 +0300

    Исправил замечания.

--- src/test/java/tax/TaxTest.java
+++ src/test/java/tax/TaxTest.java
@@ -1,10 +1,10 @@
 package tax;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TaxTest {
-    double tax = 0;
 
     /* Написать метод и тесты к нему:
        Вычислить сумму налога по прогрессивной шкале в зависимости от полученного заработка:
@@ -14,33 +14,39 @@ public class TaxTest {
        Метод должен принимать сумму оплаты труда, а возвращать сумму налога.
      */
     public double getTax(double income) {
+        double tax;
         if (income < 10000) {
-            this.tax = income * 0.13;
-            return this.tax;
+            tax = income * 0.13;
+            return tax;
         } else if (income >= 10000 && income < 50000) {
-            this.tax = income * 0.2;
-            return this.tax;
+            tax = income * 0.2;
+            return tax;
         } else {
-            this.tax = income * 0.3;
-            return this.tax;
+            tax = income * 0.3;
+            return tax;
         }
     }
 
-    @Test
-    public void testGetTax13() {
-        double result = getTax(2000);
-        Assert.assertEquals(result, 2000 * 0.13, "2000*0.13");
+    @Test(dataProvider = "taxValue")
+    public void testGetTax(double input, double expected) {
+        double result = getTax(input);
+        Assert.assertEquals(result, expected, "input is" + expected);
     }
 
-    @Test
-    public void testGetTax20() {
-        double result = getTax(20000);
-        Assert.assertEquals(result, 20000 * 0.2, "2000*0.2");
-    }
 
-    @Test
-    public void testGetTax30() {
-        double result = getTax(60000);
-        Assert.assertEquals(result, 60000 * 0.3, "2000*0.3");
+    @DataProvider
+    public Object[][] taxValue() {
+        return new Object[][]{
+                {2000, 2000 * 0.13},
+                {9999, 9999 * 0.13},
+                {10000, 10000 * 0.2},
+                {10001, 10001 * 0.2},
+                {20000, 20000 * 0.2},
+                {49999, 49999 * 0.2},
+                {50000, 50000 * 0.3},
+                {50001, 50001 * 0.3},
+                {60000, 60000 * 0.3},
+        };
+
     }
 }
